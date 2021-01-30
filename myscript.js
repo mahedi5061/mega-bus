4bb29e25f3706534da8f8026fcd4d8963538e059
mahedi5061 <mahedi5061@gmail.com>
2021-01-30 13:23:06 +0600

    online ticket count

--- myscript.js
+++ myscript.js
@@ -1,5 +1,6 @@
  
-//using one function
+//This function is used to count the ticket.
+
 function changeTicketItem(ticket,ticketNumber){
 
     const ticketCount=document.getElementById(ticket+'-count');
@@ -16,11 +17,12 @@ function changeTicketItem(ticket,ticketNumber){
 
     totalCalculate();
 }
+//This function is used to calculate the total amount.
 
 function totalCalculate(){
    
-    const firstClassTotalAmount=getTicket('firstClass');
-    const economyTotalAmount=getTicket('economy');
+    const firstClassTotalAmount=getTicketInput('firstClass');
+    const economyTotalAmount=getTicketInput('economy');
     const totalCalculateAmount=firstClassTotalAmount*150 + economyTotalAmount*100;
     document.getElementById('total-count').innerText='$' +totalCalculateAmount;
     
@@ -29,10 +31,30 @@ function totalCalculate(){
 
     const grandTotal=totalCalculateAmount+tax;
     document.getElementById('grand-total').innerText='$'+grandTotal;
+
+
+    document.getElementById('totalPay').innerText='$'+grandTotal; //This line indicate inside book now total amount.
+    
+     
 }
 
-function getTicket(ticket){
+function getTicketInput(ticket){
     const ticketTotalAmount=document.getElementById(ticket+'-count');
     const ticketTotalAmountNumber=parseInt(ticketTotalAmount.value);
     return ticketTotalAmountNumber;
 }
+
+// Ticket Booking part
+
+const bookedArea = document.getElementById('bookedTicked').addEventListener('click',function(){
+     const bookedAreaHide=document.getElementById('areaHide');
+     bookedAreaHide.style.display="none";
+
+     const paymentArea=document.getElementById('areaShow');
+     paymentArea.style.display="block";
+
+     const payCount=document.getElementById('totalPay').innerText;
+     const payCountNumber=parseInt(payCount);
+     totalCalculate();
+
+})
